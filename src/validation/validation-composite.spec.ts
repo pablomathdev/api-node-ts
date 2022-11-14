49260e953654255e071ec0868706992c5c496918
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-13 22:26:50 -0400

    test: ensure ValidationComposite calls emailValidation with correct email

--- src/validation/validation-composite.spec.ts
+++ src/validation/validation-composite.spec.ts
@@ -4,23 +4,34 @@ import { Validation } from '../presentation/interfaces/validation'
 import { ValidationComposite } from './validation-composite'
 
 const makeRequiredFields = (): Validation => {
-  class RequiredFieldsStub implements Validation {
+  class RequiredFieldsValidationStub implements Validation {
     validate (input: any): any {
       return null
     }
   }
-  return new RequiredFieldsStub()
+  return new RequiredFieldsValidationStub()
+}
+
+const makeEmailValidation = (): Validation => {
+  class EmailValidationStub implements Validation {
+    validate (input: any): any {
+      return null
+    }
+  }
+  return new EmailValidationStub()
 }
 
 type SutTypes = {
   sut: Validation
   requiredFieldsStub: Validation
+  emailValidationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
+  const emailValidationStub = makeEmailValidation()
   const requiredFieldsStub = makeRequiredFields()
-  const sut = new ValidationComposite(requiredFieldsStub)
-  return { sut, requiredFieldsStub }
+  const sut = new ValidationComposite(requiredFieldsStub, emailValidationStub)
+  return { sut, requiredFieldsStub, emailValidationStub }
 }
 
 describe('Validation Composite', () => {
@@ -42,7 +53,6 @@ describe('Validation Composite', () => {
     const { sut, requiredFieldsStub } = makeSut()
     jest.spyOn(requiredFieldsStub, 'validate')
       .mockImplementationOnce(() => { return new MissingParamError('any_field') })
-
     const fields: HttpRequest = {
       body: {
         name: 'any_name',
@@ -52,5 +62,19 @@ describe('Validation Composite', () => {
     }
     const error = sut.validate(fields.body)
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+  test('should calls emailValidation with correct email', () => {
+    const { sut, emailValidationStub } = makeSut()
+    const validateSpy = jest.spyOn(emailValidationStub, 'validate')
+
+    const httpReq: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    sut.validate(httpReq.body)
+    expect(validateSpy).toHaveBeenCalledWith(httpReq.body.email)
   })
 })

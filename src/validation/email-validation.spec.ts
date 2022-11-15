40694463b0e4c39bb123df76d92445231ae90175
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-15 11:28:38 -0400

    test: ensure EmailValidation return a error if email to invalid

--- src/validation/email-validation.spec.ts
+++ src/validation/email-validation.spec.ts
@@ -1,10 +1,13 @@
+import { InvalidEmail } from '../presentation/helpers/errors'
 import { Validation } from '../presentation/interfaces/validation'
 
 class EmailValidation implements Validation {
   constructor (private readonly emailvalidator: EmailValidator) {}
   validate (input: any): any {
     const email = input.email
-    this.emailvalidator.isValid(email)
+    if (!this.emailvalidator.isValid(email)) {
+      return new InvalidEmail()
+    }
   }
 }
 
@@ -14,8 +17,8 @@ interface EmailValidator {
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub {
-    isValid (): boolean {
-      return null
+    isValid (email: string): boolean {
+      return true
     }
   }
   return new EmailValidatorStub()
@@ -33,12 +36,21 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Email Validation', () => {
-  test('should calls EmailValidator with correct values', () => {
+  test('should calls EmailValidator with correct email', () => {
     const { sut, emailValidatorStub } = makeSut()
     const isvalidSpy = jest.spyOn(emailValidatorStub, 'isValid')
     const email = { email: 'any_email' }
 
     sut.validate(email)
     expect(isvalidSpy).toHaveBeenCalledWith('any_email')
+  })
+  test('should return error if email to invalid', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid')
+      .mockReturnValueOnce(false)
+    const email = { email: 'invalid_email' }
+
+    const result = sut.validate(email)
+    expect(result).toEqual(new InvalidEmail())
   })
 })

1c0fa9f20183cad629d8e145296a281e03fcb8a8
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-18 19:08:01 -0400

    test: make sure Authenticate calls encrypter with correct value

--- src/authentication/authenticate.spec.ts
+++ src/authentication/authenticate.spec.ts
@@ -1,17 +1,20 @@
 import { User } from '../domain/entitys/user'
+import { Encrypter } from '../domain/useCases/security/encrypter'
 import { HasherCompare } from '../domain/useCases/security/hasherCompare'
 import { Authentication } from '../domain/useCases/user/authentication'
 import { FindUserByEmail } from '../domain/useCases/user/find-user-by-email'
 
 class Authenticate implements Authentication {
   constructor (private readonly findUserByEmail: FindUserByEmail,
-    private readonly comparePassword: HasherCompare) {}
+    private readonly comparePassword: HasherCompare,
+    private readonly encrypter: Encrypter) {}
 
   async auth (email: string, password: string): Promise<string> {
     const user = await this.findUserByEmail.findByEmail(email)
     if (user) {
       await this.comparePassword.compare(password, user.password)
     }
+    await this.encrypter.encrypt(user.id)
 
     return null
   }
@@ -25,7 +28,7 @@ const makeFindUserByEmailRepository = (): FindUserByEmail => {
         email: 'any_email',
         password: 'hashed_password'
       }
-      return user
+      return new Promise(resolve => resolve(user))
     }
   }
   return new FindUserByEmailRepositoryStub()
@@ -39,12 +42,21 @@ const makeComparePassword = (): HasherCompare => {
   }
   return new ComparePasswordStub()
 }
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return new Promise(resolve => resolve('token'))
+    }
+  }
+  return new EncrypterStub()
+}
 
 const makeSut = (): any => {
+  const encrypterStub = makeEncrypter()
   const comparePasswordStub = makeComparePassword()
   const findUserByEmailRepositoryStub = makeFindUserByEmailRepository()
-  const sut = new Authenticate(findUserByEmailRepositoryStub, comparePasswordStub)
-  return { sut, findUserByEmailRepositoryStub, comparePasswordStub }
+  const sut = new Authenticate(findUserByEmailRepositoryStub, comparePasswordStub, encrypterStub)
+  return { sut, findUserByEmailRepositoryStub, comparePasswordStub, encrypterStub }
 }
 
 describe('Authentication', () => {
@@ -101,5 +113,17 @@ describe('Authentication', () => {
 
     const result = sut.auth(user.email, user.password)
     await expect(result).rejects.toThrow()
+  })
+  test('should call Encrypter with correct value', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+
+    const user = {
+      email: 'any_email',
+      password: 'hashed_password'
+    }
+
+    await sut.auth(user.email, user.password)
+    expect(encryptSpy).toHaveBeenCalledWith('any_id')
   })
 })

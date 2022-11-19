5dcd61f15160b4f792f825df65d678869905ae9e
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-19 12:53:54 -0400

    test: make sure AddUserTokenRepository calls database repository with correct values

---
+++ src/repositories/add-user-token.spec.ts
@@ -0,0 +1,30 @@
+
+import { AddUserToken } from '../domain/useCases/token/add-user-token'
+
+interface AddTokenInDatabase {
+  add(value: string, token: string): Promise<void>
+}
+
+class DatabaseRepository implements AddTokenInDatabase {
+  async add (value: string, token: string): Promise<void> {
+
+  }
+}
+
+class AddUserTokenRepository implements AddUserToken {
+  constructor (private readonly addTokenInDatabase: AddTokenInDatabase) {}
+  async addToken (value: string, token: string): Promise<void> {
+    await this.addTokenInDatabase.add(value, token)
+  }
+}
+
+describe('add user token repository', () => {
+  test('should database repository with correct values', async () => {
+    const databaseRepository = new DatabaseRepository()
+    const sut = new AddUserTokenRepository(databaseRepository)
+    const addSpy = jest.spyOn(databaseRepository, 'add')
+    const values = { user_id: 'user_id', token: 'token' }
+    await sut.addToken(values.user_id, values.token)
+    expect(addSpy).toHaveBeenCalledWith('user_id', 'token')
+  })
+})

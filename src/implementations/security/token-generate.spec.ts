207875f01bbe423b5a7b6a1d62257806104accbb
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-19 11:39:29 -0400

    test: make sure TokenGeneratorImplementation throw if jsowebtoken throws

--- src/implementations/security/token-generate.spec.ts
+++ src/implementations/security/token-generate.spec.ts
@@ -1,8 +1,17 @@
 import { TokenGenerator } from '../../domain/useCases/security/token-generator'
 import Jwt from 'jsonwebtoken'
 class TokenGeneratorImplementation implements TokenGenerator {
+  static get options (): any {
+    return {
+      expiresIn: 3600000,
+      secretKey: 'secret'
+    }
+  }
+
   async generate (value: string): Promise<string> {
-    await Jwt.sign(value, 'secretkey')
+    await Jwt.sign({ user_id: value },
+      TokenGeneratorImplementation.options.secretKey,
+      { expiresIn: TokenGeneratorImplementation.options.expiresIn })
     return null
   }
 }
@@ -12,6 +21,12 @@ describe('Token Generate', () => {
     const sut = new TokenGeneratorImplementation()
     const signSpy = jest.spyOn(Jwt, 'sign')
     await sut.generate('user_id')
-    expect(signSpy).toHaveBeenCalledWith('user_id', 'secretkey')
+    expect(signSpy).toHaveBeenCalledWith({ user_id: 'user_id' }, 'secret', { expiresIn: 3600000 })
+  })
+  test('should throw if jsonwebtoken throws', async () => {
+    const sut = new TokenGeneratorImplementation()
+    jest.spyOn(Jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const result = sut.generate('user_id')
+    await expect(result).rejects.toThrow()
   })
 })

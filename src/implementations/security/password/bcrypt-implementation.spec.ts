e747b0b31c41f79b83c534fc6863f81950d2f341
Pablo Matheus <pablomatheus171@gmail.com>
2022-11-25 19:19:17 -0400

    test: make sure Bcrypt Implementation passes correct values to Bcrypt

--- src/implementations/security/password/bcrypt-implementation.spec.ts
+++ src/implementations/security/password/bcrypt-implementation.spec.ts
@@ -1,18 +1,50 @@
-// import { Hasher } from '../../../domain/security/Hasher'
-// import { HasherCompare } from '../../../domain/security/hasherCompare'
+import { Hasher } from '../../../domain/security/Hasher'
+import { HasherCompare } from '../../../domain/security/hasherCompare'
+import bcrypt from 'bcrypt'
 
-// class BcryptImplementation implements Hasher, HasherCompare {
-//   async hash (value: string): Promise<string> {
-//     throw new Error('Method not implemented.')
+// jest.mock('bcrypt', () => {
+//   return {
+//     async hash (): Promise<string> {
+//       return new Promise(resolve => resolve('hashed_password'))
+//     }
 //   }
+// })
+jest.mock('bcrypt')
 
-//   async compare (value: string, hashedValue: string): Promise<boolean> {
-//     throw new Error('Method not implemented.')
-//   }
-// }
+class BcryptImplementation implements Hasher, HasherCompare {
+  async hash (value: string): Promise<string> {
+    const salt = 10
+    const hashedPassword = await bcrypt.hash(value, salt)
+    return hashedPassword
+  }
+
+  async compare (value: string, hashedValue: string): Promise<boolean> {
+    await bcrypt.compare(value, hashedValue)
+    return null
+  }
+}
 
 describe('Bcrypt', () => {
-  test('should call bcrypt with correct values', () => {
-
+  test('should call bcrypt with correct values', async () => {
+    const sut = new BcryptImplementation()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.hash('user_password')
+    await sut.compare('user_password', 'hashed_password')
+    expect(hashSpy).toHaveBeenCalledWith('user_password', 10)
+    expect(compareSpy).toHaveBeenCalledWith('user_password', 'hashed_password')
   })
+  //   test('should returns hashed password if bcrypt hash success', async () => {
+  //     const sut = new BcryptImplementation()
+
+  //     const result = await sut.hash('user_password')
+  //     expect(result).toBe('hashed_password')
+  //   })
+  //   test('should call bcrypt with correct values', async () => {
+  //     const sut = new BcryptImplementation()
+  //     const hashSpy = jest.spyOn(bcrypt, 'hash')
+
+//     await sut.hash('user_password')
+//     expect(hashSpy).toHaveBeenCalledWith('user_password', 10)
+//   })
 })
